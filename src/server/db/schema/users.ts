7ed9043d3ceb8ce538d6ec66e88e1f836b9978e3
ahargunyllib <billy.bpm03@gmail.com>
2025-05-27 21:51:10 +0700

    fix: set default value for user ID to random UUID for improved uniqueness

--- src/server/db/schema/users.ts
+++ src/server/db/schema/users.ts
@@ -8,7 +8,7 @@ import {
 } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-	id: uuid("id").primaryKey(),
+	id: uuid("id").primaryKey().defaultRandom(),
 	firstName: varchar("first_name", { length: 255 }).notNull(),
 	lastName: varchar("last_name", { length: 255 }).notNull(),
 	email: varchar({ length: 255 }).notNull().unique(),
