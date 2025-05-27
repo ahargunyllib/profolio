b53c74e228523b5068340e0afda2082c3f3f0e34
ahargunyllib <billy.bpm03@gmail.com>
2025-05-27 21:12:57 +0700

    feat: enhance user schema and add CV schema with additional fields

---
+++ src/server/db/schema/cv.ts
@@ -0,0 +1,26 @@
+import {
+	integer,
+	json,
+	pgTable,
+	timestamp,
+	uuid,
+	varchar,
+} from "drizzle-orm/pg-core";
+import { usersTable } from "./users";
+
+export const cvsTable = pgTable("cvs", {
+	id: uuid("id").primaryKey(),
+	userId: uuid("user_id")
+		.notNull()
+		.references(() => usersTable.id),
+	jobName: varchar("job_name").notNull(),
+	atsScore: integer("ats_score").notNull().default(0),
+	status: integer().notNull().default(1), // 1: draft, 2: needs review, 3: completed
+	suggestions: json().notNull(),
+	data: json().notNull(),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.notNull()
+		.$onUpdateFn(() => new Date()),
+});
--- src/server/db/schema/users.ts
+++ src/server/db/schema/users.ts
@@ -1,6 +1,7 @@
 import {
 	integer,
 	pgTable,
+	text,
 	timestamp,
 	uuid,
 	varchar,
@@ -8,10 +9,18 @@ import {
 
 export const usersTable = pgTable("users", {
 	id: uuid("id").primaryKey(),
-	name: varchar({ length: 255 }).notNull(),
+	firstName: varchar("first_name", { length: 255 }).notNull(),
+	lastName: varchar("last_name", { length: 255 }).notNull(),
 	email: varchar({ length: 255 }).notNull().unique(),
 	role: integer().notNull().default(1), // 1: user
 	password: varchar({ length: 255 }).notNull(),
+	profilePictureURL: varchar("profile_picture_url", { length: 255 }),
+	phoneNumber: varchar("phone_number", { length: 20 }),
+	location: varchar("location", { length: 255 }),
+	website: varchar("website", { length: 255 }),
+	bio: text("bio"),
+	currentJobTitle: varchar("current_job_title", { length: 255 }),
+	currentCompany: varchar("current_company", { length: 255 }),
 	createdAt: timestamp("created_at").defaultNow().notNull(),
 	updatedAt: timestamp("updated_at")
 		.defaultNow()
