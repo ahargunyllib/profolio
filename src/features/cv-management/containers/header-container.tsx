d4ab1a9d0dd123d60dafeeb5bc450d8eef2defab
ahargunyllib <billy.bpm03@gmail.com>
2025-06-03 00:31:39 +0700

    feat: implement Account Settings page with password change and account deletion features

--- src/features/cv-management/containers/header-container.tsx
+++ src/features/cv-management/containers/header-container.tsx
@@ -5,11 +5,23 @@ import {
 	AvatarFallback,
 	AvatarImage,
 } from "@/shared/components/ui/avatar";
-import { useGetMySessionQuery } from "@/shared/repositories/auth/query";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/shared/components/ui/dropdown-menu";
+import {
+	useGetMySessionQuery,
+	useLogoutMutation,
+} from "@/shared/repositories/auth/query";
+import { LogOutIcon, SettingsIcon, User2Icon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function HeaderContainer() {
 	const { data: res, isLoading } = useGetMySessionQuery();
+	const { mutate: logout, isPending } = useLogoutMutation();
 
 	return (
 		<header className="py-4 px-8 md:py-8 md:px-16 border-b flex flex-row items-center justify-between">
@@ -24,19 +36,45 @@ export default function HeaderContainer() {
 						{!isLoading && res?.success && res.data.firstName}
 					</span>
 				</span>
-				<Avatar>
-					<AvatarImage
-						src={
-							(!isLoading && res?.success && res.data.profilePictureURL) ||
-							undefined
-						}
-						alt="Profile Picture"
-					/>
-					<AvatarFallback>
-						{!isLoading && res?.success && res.data.firstName[0]}
-						{!isLoading && res?.success && res.data.lastName[0]}
-					</AvatarFallback>
-				</Avatar>
+				<DropdownMenu>
+					<DropdownMenuTrigger>
+						<Avatar>
+							<AvatarImage
+								src={
+									(!isLoading && res?.success && res.data.profilePictureURL) ||
+									undefined
+								}
+								alt="Profile Picture"
+							/>
+							<AvatarFallback>
+								{!isLoading && res?.success && res.data.firstName[0]}
+								{!isLoading && res?.success && res.data.lastName[0]}
+							</AvatarFallback>
+						</Avatar>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuItem asChild>
+							<Link href="/dashboard/profile">
+								<User2Icon />
+								Profile
+							</Link>
+						</DropdownMenuItem>
+						<DropdownMenuItem asChild>
+							<Link href="/dashboard/settings">
+								<SettingsIcon />
+								Setting
+							</Link>
+						</DropdownMenuItem>
+						<DropdownMenuItem
+							variant="destructive"
+							onClick={() => logout()}
+							disabled={isPending}
+						>
+							<LogOutIcon />
+							Logout
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
 			</div>
 		</header>
 	);
